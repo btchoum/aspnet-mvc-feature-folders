8c9f65bd1576ae9397c7ab40a59f634e4691e478
btchoum <bertrand.tchoumkeu@gmail.com>
2018-01-14 12:14:57 -0600

    added support for feature folders

--- TicketTracker/TicketTracker.Web/Global.asax.cs
+++ TicketTracker/TicketTracker.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TicketTracker.Web.Infrastructure;
 
 namespace TicketTracker.Web
 {
@@ -18,6 +19,10 @@ namespace TicketTracker.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Apply Feature folders view location
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new FeatureFoldersRazorViewEngine());
         }
     }
 }
---
+++ TicketTracker/TicketTracker.Web/Infrastructure/FeatureFoldersRazorViewEngine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace TicketTracker.Web.Infrastructure
+{
+    public class FeatureFoldersRazorViewEngine : RazorViewEngine
+    {
+        public FeatureFoldersRazorViewEngine()
+        {
+            var features = new string[] { "Tickets" };
+            var formats = new List<string>
+            {
+                "~/Features/{1}/{0}.cshtml",
+                "~/Features/SharedViews/{0}.cshtml"
+            };
+
+            foreach (var feat in features)
+            {
+                formats.Add(string.Format("~/Features/{0}/{{1}}/{{0}}.cshtml", feat));
+            }
+
+            var featureFolderViewLocationFormats = formats.ToArray();
+
+            ViewLocationFormats = featureFolderViewLocationFormats;
+            MasterLocationFormats = featureFolderViewLocationFormats;
+            PartialViewLocationFormats = featureFolderViewLocationFormats;
+        }
+    }
+}
